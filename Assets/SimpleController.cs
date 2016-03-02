f941cb2a2ea7ee670cb24a0c39cd89d253fb722e
John Harger <john.harger@gmail.com>
2016-03-01 19:22:16 -0700

    Added code for workshop 2

---
+++ Assets/SimpleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleController : MonoBehaviour {
+    public float move = 1f;
+
+    Rigidbody body;
+
+	void Awake () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        body.velocity = move * new Vector3(h, 0, v);
+	}
+}
